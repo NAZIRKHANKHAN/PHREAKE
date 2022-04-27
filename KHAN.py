cc78bde6393934c736154bb5f6b0e189af45a42a
Khan khan <97614144+NAZIRKHANKHAN@users.noreply.github.com>
2022-04-28 01:35:57 +0500

    Add files via upload

---
+++ KHAN.py
@@ -0,0 +1,11 @@
+import os
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+os.system('git pull')
+os.system('clear')
+from KHAN import unknown
+unknown()
